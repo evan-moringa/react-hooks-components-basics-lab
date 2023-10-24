2f54ae01f890dfd2e5a29ab88dc37d24b450ecca
Evan Keya <evan.keya@student.moringaschool.com>
2023-10-24 22:24:13 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -2,29 +2,41 @@ import React from "react";
 
 function NavBar() {
   return (
+    <div id="navbar">
     <nav>
       <a href="#home">I'm a link!</a>
     </nav>
+    </div>
   );
 }
 
 function Home() {
   return (
+    
     <div id="home">
-      <h1>Home</h1>
+      
     </div>
+    
   );
 }
 
-{/* write an <About> component here */}
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+
+      <Home />
+
+      <About />
     </div>
   );
 }
